512f0756a4a5afe7518ac65f5fc0a6416646ec0f
Lybron Sobers <lybron.developer@gmail.com>
2024-08-26 11:22:54 +0200

    Resolve eslint errors in backend code

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ const PORT = 3001
 app.use('/todo', todoRouter);
 app.use('/todo-lists', todoListRouter);
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(500).send('There was an error.');
 });
